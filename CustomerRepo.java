867628753e723cbcef7991bdddb015dd66a1b472
shivamtiwariGL <110605224+shivamtiwariGL@users.noreply.github.com>
2022-08-22 11:25:50 +0530

    Add files via upload

---
+++ CustomerRepo.java
@@ -0,0 +1,11 @@
+package com.example.demo.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.demo.model.Customer;
+
+public interface CustomerRepo extends CrudRepository<Customer, Integer> 
+{
+
+
+}
