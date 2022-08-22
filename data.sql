c17176b96e93a72d527ea907936c939ac5de0af5
shivamtiwariGL <110605224+shivamtiwariGL@users.noreply.github.com>
2022-08-22 11:26:43 +0530

    Add files via upload

---
+++ data.sql
@@ -0,0 +1,5 @@
+insert into customer values (102,'Kiran', 'Mumbai', 'debug', 55555, 94567);
+insert into customer values (105,'Kiran', 'Delhi', 'bug', 22222, 943526);
+insert into customer values (108,'Kiran', 'Bangalore', 'newton', 1111, 87654);
+
+
