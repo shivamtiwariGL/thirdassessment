1dcad94625b052b6f434b8a3b0ec708f3887ad53
shivamtiwariGL <110605224+shivamtiwariGL@users.noreply.github.com>
2022-08-22 11:25:15 +0530

    Add files via upload

---
+++ Sbq2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sbq2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sbq2Application.class, args);
+	}
+
+}
